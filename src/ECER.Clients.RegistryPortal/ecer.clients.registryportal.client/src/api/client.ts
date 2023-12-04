9de264641eb519ec3bb16775289a646d47731ed2
Parker Atkins <parker.cattell.atkins@gmail.com>
2023-12-04 13:30:43 -0800

    Add Bearer token to axios requests

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/client.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/client.ts
@@ -1,5 +1,5 @@
 import OpenAPIClientAxios from "openapi-client-axios";
-
+import { useUserStore } from "@/store/user";
 import type { Client } from "@/types/openapi";
 
 export const getClient = async () => {
@@ -7,5 +7,17 @@ export const getClient = async () => {
     definition: `${import.meta.env.VITE_BASE_URL}/swagger/v1/swagger.json`,
   });
 
-  return await api.init<Client>();
+  const axiosClient = await api.init<Client>();
+
+  // Add a request interceptor to append the access token to the request
+  axiosClient.interceptors.request.use((config) => {
+    const userStore = useUserStore();
+    const token = userStore.userInfo?.access_token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return axiosClient;
 };
