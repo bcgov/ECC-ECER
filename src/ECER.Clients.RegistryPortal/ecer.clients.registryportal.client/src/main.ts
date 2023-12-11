2d179984f2da193b1e1416df1a66629e35ed34a0
Parker Atkins <parker.cattell.atkins@gmail.com>
2023-12-11 13:33:55 -0800

    Install vuetify

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/main.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/main.ts
@@ -1,10 +1,21 @@
+// Vuetify
+import "vuetify/styles";
+
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
 import router from "./router";
 import { useConfigStore } from "./store/config";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
@@ -13,6 +24,7 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(vuetify);
 
 const configStore = useConfigStore();
 
