b97bd7054859e31cd6149cda9b0ea199fe2afb89
Parker Atkins <parker.cattell.atkins@gmail.com>
2023-12-11 15:28:39 -0800

    Setup preliminary styling
    * Install vuetify
    
    * Color theming vuetify
    
    * Fonts and preliminary icons

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/main.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/main.ts
@@ -1,10 +1,28 @@
+// Vuetify
+import "vuetify/styles";
+
 import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
 import router from "./router";
 import { useConfigStore } from "./store/config";
+import ecerTheme from "./styles/ecer-theme";
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "ecerTheme",
+    themes: {
+      ecerTheme,
+    },
+  },
+  components,
+  directives,
+});
 
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
@@ -13,6 +31,7 @@ const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(vuetify);
 
 const configStore = useConfigStore();
 
