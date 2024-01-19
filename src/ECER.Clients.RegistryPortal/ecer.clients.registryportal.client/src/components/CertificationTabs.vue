86b92ad15b6b20a65c01ea68ebe206a7fec2b564
soletsdev <soletsdev@github>
2024-01-19 13:36:26 -0800

    quick cleanup code formatting

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/CertificationTabs.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/CertificationTabs.vue
@@ -3,17 +3,21 @@
   <v-expansion-panels v-if="$vuetify.display.mobile" :model-value="expansionPanelIndex" @update:model-value="updateTab">
     <v-expansion-panel elevation="0">
       <v-expansion-panel-title>
-        <v-icon size="large" icon="mdi-progress-check" class="mr-2"></v-icon>
+        <v-icon size="large" icon="mdi-progress-check" class="mr-2" />
         In Progress ({{ applicationStore.inProgressCount }})
       </v-expansion-panel-title>
-      <v-expansion-panel-text><router-view class="mt-4"></router-view></v-expansion-panel-text>
+      <v-expansion-panel-text>
+        <router-view class="mt-4" />
+      </v-expansion-panel-text>
     </v-expansion-panel>
     <v-expansion-panel elevation="0">
       <v-expansion-panel-title>
-        <v-icon size="large" icon="mdi-check-circle-outline" class="mr-2"></v-icon>
+        <v-icon size="large" icon="mdi-check-circle-outline" class="mr-2" />
         Completed ({{ applicationStore.completedCount }})
       </v-expansion-panel-title>
-      <v-expansion-panel-text><router-view class="mt-4"></router-view></v-expansion-panel-text>
+      <v-expansion-panel-text>
+        <router-view class="mt-4" />
+      </v-expansion-panel-text>
     </v-expansion-panel>
   </v-expansion-panels>
 
