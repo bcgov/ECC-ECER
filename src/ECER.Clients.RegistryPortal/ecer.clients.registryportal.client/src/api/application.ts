57d90aa22bb1df38d25a4095bdc609af977f9c59
ytqsl <yossi.tamari@live.com>
2024-01-24 07:40:19 -0800

    fixed post application parameter

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/application.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/application.ts
@@ -6,9 +6,9 @@ const getApplications = async (): Promise<Components.Schemas.Application[] | nul
   return (await client.application_get()).data.items;
 };
 
-const postApplication = async (): Promise<string | null | undefined> => {
+const postApplication = async (applicationId: string): Promise<string | null | undefined> => {
   const client = await getClient();
-  return (await client.application_post({}, {})).data.applicationId;
+  return (await client.application_post(applicationId, {})).data.applicationId;
 };
 
 export { getApplications, postApplication };
