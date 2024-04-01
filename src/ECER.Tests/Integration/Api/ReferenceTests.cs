e35cc990385b8bae5415e030502e368e750b1986
Farzad <farzad.alankesh@quartech.com>
2024-04-01 15:30:03 -0700

    ECER-1402: Automated tests added for managers

--- src/ECER.Tests/Integration/Api/ReferenceTests.cs
+++ src/ECER.Tests/Integration/Api/ReferenceTests.cs
@@ -16,7 +16,7 @@ public class ReferenceTests : ApiWebAppScenarioBase
   [Fact]
   public async Task GenerateReferenceLink_WithValidInfo_ReturnsOk()
   {
-    var validRequest = new GenerateReferenceLinkRequest(Guid.NewGuid(), ReferenceType.CharacterReference);
+    var validRequest = new PortalInvitationToLinkRequest(Guid.NewGuid(), ReferenceType.CharacterReference);
     await Host.Scenario(_ =>
     {
       _.Post.Json(validRequest).ToUrl("/api/references");
@@ -28,7 +28,7 @@ public class ReferenceTests : ApiWebAppScenarioBase
   public async Task CanTransform()
   {
     var bus = Host.Services.GetRequiredService<IMediator>();
-    var response = await bus.Send(new ReferenceLinkQuery(Guid.NewGuid(), ReferenceType.CharacterReference), CancellationToken.None);
+    var response = await bus.Send(new GenerateReferenceLinkCommand(Guid.NewGuid(), ReferenceType.CharacterReference), CancellationToken.None);
     response.ShouldNotBeNull();
   }
 }
--- src/ECER.Tests/Integration/Managers/Admin/ReferenceHandlersTests.cs
+++ src/ECER.Tests/Integration/Managers/Admin/ReferenceHandlersTests.cs
@@ -17,7 +17,20 @@ public class ReferenceHandlersTests : ApiWebAppScenarioBase
   public async Task CanTransform()
   {
     var bus = Host.Services.GetRequiredService<IMediator>();
-    var response = await bus.Send(new ReferenceLinkQuery(Guid.NewGuid(), ReferenceType.CharacterReference), CancellationToken.None);
+    var response = await bus.Send(new GenerateReferenceLinkCommand(Guid.NewGuid(), ReferenceType.CharacterReference), CancellationToken.None);
     response.ShouldNotBeNull();
+  }
+
+  [Fact]
+  public async Task CanUnTransform()
+  {
+    var bus = Host.Services.GetRequiredService<IMediator>();
+    var guid = Guid.NewGuid();
+    var packingResponse = await bus.Send(new GenerateReferenceLinkCommand(guid, ReferenceType.WorkExperienceReference), CancellationToken.None);
+    packingResponse.ShouldNotBeNull();
+
+    var unpackingResponse = await bus.Send(new UnpackReferenceLinkCommand(guid, packingResponse.referenceLink), CancellationToken.None);
+    unpackingResponse.portalInvitation.ShouldBe(guid);
+    unpackingResponse.referenceType.ShouldBe(ReferenceType.WorkExperienceReference);
   }
 }
