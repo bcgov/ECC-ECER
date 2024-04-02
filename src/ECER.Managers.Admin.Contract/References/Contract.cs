0464d5eb057a70dbd70514edca8a7df83f662cf8
Farzad <farzad.alankesh@quartech.com>
2024-04-02 09:44:31 -0700

    ECER-1402: Contract parameter name edited

--- src/ECER.Managers.Admin.Contract/References/Contract.cs
+++ src/ECER.Managers.Admin.Contract/References/Contract.cs
@@ -3,10 +3,10 @@
 namespace ECER.Managers.Admin.Contract.References;
 
 public record GenerateReferenceLinkCommand(Guid portalInvitation, ReferenceType referenceType) : IRequest<PortalInvitationToLinkResponse>;
-public record UnpackReferenceLinkCommand(Guid portalInvitation, string referenceLink) : IRequest<LinkToPortalInvitationResponse>;
+public record UnpackReferenceLinkCommand(Guid portalInvitation, string encryptedVerificationToken) : IRequest<LinkToPortalInvitationResponse>;
 public record PortalInvitationToLinkRequest(Guid portalInvitation, ReferenceType referenceType);
-public record PortalInvitationToLinkResponse(Guid portalInvitation, string referenceLink);
-public record LinkToPortalInvitationRequest(Guid portalInvitation, string referenceLink);
+public record PortalInvitationToLinkResponse(Guid portalInvitation, string encryptedVerificationToken);
+public record LinkToPortalInvitationRequest(Guid portalInvitation, string encryptedVerificationToken);
 public record LinkToPortalInvitationResponse(Guid portalInvitation, ReferenceType referenceType);
 
 public enum ReferenceType
