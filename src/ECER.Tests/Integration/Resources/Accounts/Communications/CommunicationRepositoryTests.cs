82facc86f835b329a234e6e7156d3c180647ea76
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-04-24 10:52:46 -0700

    Repository test

--- src/ECER.Tests/Integration/Resources/Accounts/Communications/CommunicationRepositoryTests.cs
+++ src/ECER.Tests/Integration/Resources/Accounts/Communications/CommunicationRepositoryTests.cs
@@ -1,8 +1,11 @@
-﻿using ECER.Resources.Accounts.Communications;
+﻿using ECER.Managers.Registry.Contract.Communications;
+using ECER.Resources.Accounts.Communications;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 using Xunit.Abstractions;
 using Xunit.Categories;
+using CommunicationStatus = ECER.Resources.Accounts.Communications.CommunicationStatus;
+using UserCommunicationQuery = ECER.Resources.Accounts.Communications.UserCommunicationQuery;
 
 namespace ECER.Tests.Integration.Resources.Accounts.Communications;
 
@@ -27,5 +30,26 @@ public class CommunicationRepositoryTests : RegistryPortalWebAppScenarioBase
 
     // Assert
     communications.ShouldHaveSingleItem();
+  }
+  
+  [Fact]
+  public async Task SeenCommunications_MarkAsSeen()
+  {
+    var communicationId = Fixture.communicationId;
+    
+    // Ensure test message is not "seen"
+    var communications = await repository.Query(new UserCommunicationQuery { ById = communicationId });
+    communications.ShouldHaveSingleItem();
+    communications.First().Acknowledged.ShouldBeFalse();
+    communications.First().Status.ShouldBe(CommunicationStatus.NotifiedRecipient);
+
+    // Act
+    await repository.Seen(communicationId, default);
+    var seenCommunications = await repository.Query(new UserCommunicationQuery { ById = communicationId });
+
+    // Assert communication has been marked "seen"
+    seenCommunications.ShouldHaveSingleItem();
+    seenCommunications.First().Acknowledged.ShouldBeTrue();
+    seenCommunications.First().Status.ShouldBe(CommunicationStatus.Acknowledged);
   }
 }
