b815165197803e55280795fdd2406e73121ca2ff
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-04-30 14:13:47 -0700

    Mobile view for Message component

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/Message.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/Message.vue
@@ -1,11 +1,33 @@
 <template>
-  <h3>{{ messageStore.currentMessage?.subject }}</h3>
-  <p class="small mt-2">{{ messageDate }}</p>
-  <p class="small mt-6">{{ messageStore.currentMessage?.text }}</p>
+  <div v-if="smAndDown">
+    <v-dialog
+      :model-value="smAndDown && messageStore.currentMessage != null"
+      :transition="false"
+      fullscreen
+      @update:model-value="messageStore.currentMessage = null"
+    >
+      <v-card>
+        <v-toolbar color="white">
+          <v-btn prepend-icon="mdi-close" text="Close" @click="messageStore.currentMessage = null"></v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <h3>{{ messageStore.currentMessage?.subject }}</h3>
+          <p class="small mt-2">{{ messageDate }}</p>
+          <p class="small mt-6">{{ messageStore.currentMessage?.text }}</p>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+  <div v-if="mdAndUp">
+    <h3>{{ messageStore.currentMessage?.subject }}</h3>
+    <p class="small mt-2">{{ messageDate }}</p>
+    <p class="small mt-6">{{ messageStore.currentMessage?.text }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useDisplay } from "vuetify";
 
 import { useMessageStore } from "@/store/message";
 import { formatDate } from "@/utils/format";
@@ -14,8 +36,9 @@ export default defineComponent({
   name: "Message",
   setup() {
     const messageStore = useMessageStore();
+    const { smAndDown, mdAndUp } = useDisplay();
 
-    return { messageStore };
+    return { messageStore, smAndDown, mdAndUp };
   },
   computed: {
     messageDate(): string {
