78ac09fb002fb9e635d217fd6911af4eb1a5e269
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-05-15 13:46:36 -0700

    Application card 'Draft', 'Submitted' and undefined states

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ApplicationCard.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ApplicationCard.vue
@@ -1,24 +1,36 @@
 <template>
-  <v-card class="rounded-lg fill-height" flat color="primary">
+  <v-card class="fill-height" flat color="primary">
     <v-card-item class="ma-4">
-      <h3 class="text-white">Apply for ECE certification</h3>
+      <h3 class="text-white">{{ title }}</h3>
       <p class="small text-white mt-4">
-        There are different types of certifications you can apply for. Visit the B.C. government website to learn about the types of Early Childhood Educator
-        (ECE) certificates and which one you may qualify for.
+        {{ subTitle }}
       </p>
     </v-card-item>
     <v-card-actions class="ma-4">
-      <div v-if="applicationStore.hasDraftApplication" class="d-flex flex-row justify-start ga-3 flex-wrap">
+      <!-- Application status DRAFT -->
+      <div v-if="applicationStore.applicationStatus === 'Draft'" class="d-flex flex-row justify-start ga-3 flex-wrap">
         <v-btn size="large" variant="flat" color="warning" @click="$router.push('/application')">
           <v-icon size="large" icon="mdi-arrow-right" />
           Open application
         </v-btn>
         <v-btn class="ma-0" size="large" variant="outlined" color="white" @click="$emit('cancel-application')">Cancel application</v-btn>
       </div>
-      <v-btn v-else variant="flat" size="large" color="warning" @click="handleStartNewApplication">
-        <v-icon size="large" icon="mdi-arrow-right" />
-        Apply Now
-      </v-btn>
+
+      <!-- Application status Submitted -->
+      <div v-if="applicationStore.applicationStatus === 'Submitted'" class="d-flex flex-row justify-start ga-3 flex-wrap">
+        <v-btn variant="flat" size="large" color="warning" @click="handleManageApplication">
+          <v-icon size="large" icon="mdi-arrow-right" />
+          Manage Application
+        </v-btn>
+      </div>
+
+      <!-- No application found -->
+      <div v-if="applicationStore.applicationStatus === undefined" class="d-flex flex-row justify-start ga-3 flex-wrap">
+        <v-btn variant="flat" size="large" color="warning" @click="handleStartNewApplication">
+          <v-icon size="large" icon="mdi-arrow-right" />
+          Apply Now
+        </v-btn>
+      </div>
     </v-card-actions>
   </v-card>
 </template>
@@ -27,21 +39,47 @@
 import { defineComponent } from "vue";
 
 import { useApplicationStore } from "@/store/application";
+import { formatDate } from "@/utils/format";
 
 export default defineComponent({
   name: "ApplicationCard",
   emits: ["cancel-application"],
   setup() {
     const applicationStore = useApplicationStore();
+
     return {
       applicationStore,
     };
   },
-  computed: {},
+  computed: {
+    title(): string {
+      switch (this.applicationStore.application?.status) {
+        case "Draft":
+          return "Application in progress";
+        case "Submitted":
+          return "Application in progress";
+        default:
+          return "Apply for ECE Certification";
+      }
+    },
+    subTitle(): string {
+      switch (this.applicationStore.application?.status) {
+        case "Draft":
+          return `Started ${formatDate(this.applicationStore.application?.createdOn || "", "LLL dd, yyyy")}`;
+        case "Submitted":
+          return `Submitted ${formatDate(this.applicationStore.application?.submittedOn || "", "LLL dd, yyyy")}`;
+        default:
+          return "There are different types of certifications you can apply for. Visit the B.C. government website to learn about the types of Early Childhood Educator (ECE) certificates and which one you may qualify for.";
+      }
+    },
+  },
   methods: {
     handleStartNewApplication() {
       this.applicationStore.upsertDraftApplication();
       this.$router.push("/application");
+    },
+    handleManageApplication() {
+      this.$router.push("/manage-application");
     },
   },
 });
