49f36540afbcdf52f341a8532c993f7ef04efd62
Deepak Yadav <Deepak.Yadav@quartech.com>
2024-05-14 20:08:37 -0700

    ECER-870 - Work Ex reference submit flow - competencies assessment

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/config/work-experience-reference-assessment-form.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/config/work-experience-reference-assessment-form.ts
@@ -1,21 +1,21 @@
-import EceWorkExperienceReferenceDetails from "@/components/reference/inputs/EceWorkExperienceReferenceDetails.vue";
+import EceWorkExperienceReferenceAssessment from "@/components/reference/inputs/EceWorkExperienceReferenceDetails.vue";
 import type { Form } from "@/types/form";
 
 const workExperienceAssessmentForm: Form = {
-  id: "workExAssessmentForm",
-  title: "Work Experience Assessment form",
-  inputs: {
-    workExAssessment: {
-      id: "workExAssessment",
-      component: EceWorkExperienceReferenceDetails,
-      props: {},
-      cols: {
-        md: 12,
-        lg: 12,
-        xl: 12,
-      },
+    id: "workExperienceAssessmentForm",
+    title: "Work Experience Assessment form",
+    inputs: {
+        workExperienceAssessment: {
+            id: "workExperienceAssessment",
+            component: EceWorkExperienceReferenceAssessment,
+            props: {},
+            cols: {
+                md: 12,
+                lg: 12,
+                xl: 12,
+            },
+        },
     },
-  },
 };
 
 export default workExperienceAssessmentForm;
--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/config/work-experience-reference-wizard.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/config/work-experience-reference-wizard.ts
@@ -1,10 +1,11 @@
-import workExperienceAssessmentForm from "@/config/work-experience-reference-assessment-form";
 import workExperienceDeclarationForm from "@/config/work-experience-reference-declaration-form";
 import workExperienceDeclineForm from "@/config/work-experience-reference-decline-form";
 import workExperienceReviewForm from "@/config/work-experience-reference-review-form";
 import type { Wizard } from "@/types/wizard";
 
 import referenceContactInformationForm from "./reference-contact-information-form";
+import workExperienceAssessmentForm from "./work-experience-reference-assessment-form";
+import workExperienceEvaluationForm from "./work-experience-reference-evaluation-form";
 
 const workExperienceReferenceWizardConfig: Wizard = {
   id: "workReferenceWizard",
@@ -21,11 +22,17 @@ const workExperienceReferenceWizardConfig: Wizard = {
       form: referenceContactInformationForm,
       key: "item.2",
     },
+    workExperienceEvaluation: {
+      title: "",
+      stage: "ReferenceEvaluation",
+      form: workExperienceEvaluationForm,
+      key: "item.3",
+    },
     assessment: {
       title: "",
       stage: "Assessment",
       form: workExperienceAssessmentForm,
-      key: "item.3",
+      key: "item.4",
     },
     review: {
       title: "Review",
@@ -37,7 +44,7 @@ const workExperienceReferenceWizardConfig: Wizard = {
       title: "",
       stage: "Decline",
       form: workExperienceDeclineForm,
-      key: "item.5",
+      key: "item.6",
     },
   },
 };
