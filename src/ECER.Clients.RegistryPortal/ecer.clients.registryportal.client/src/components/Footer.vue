2c6a443b789f3f672ba6baf7b949c30ce683bbf2
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-05-16 14:03:55 -0700

    Draft Footer changes

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/Footer.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/Footer.vue
@@ -1,12 +1,26 @@
 <template>
-  <v-footer app color="primary" absolute :style="{ 'min-height': '50px' }">
-    <v-row justify="start" class="ga-1 ms-2 my-1">
-      <router-link v-for="link in links" :key="link.name" :to="link.path">
-        <v-btn variant="text">
-          <p class="white small">{{ link.name }}</p>
-        </v-btn>
-      </router-link>
-    </v-row>
+  <v-footer border app absolute :style="{ 'min-height': '50px' }">
+    <v-container>
+      <div class="d-flex w-100 justify-space-between">
+        <router-link to="/">
+          <img src="../assets/bc-gov-logo.png" width="155" class="logo ms-6" alt="B.C. Government Logo" />
+        </router-link>
+        <div class="w-lg-25 w-50">
+          <p><b>More information</b></p>
+          <div class="d-flex">
+            <div class="d-flex flex-column w-50">
+              <a v-for="link in firstColumnLinks" :key="link.name" :href="link.path" class="small text-black">{{ link.name }}</a>
+            </div>
+            <div class="d-flex flex-column w-50">
+              <a v-for="link in secondColumnLinks" :key="link.name" :href="link.path" class="small text-black">{{ link.name }}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <v-divider class="border-opacity-100 w-100 my-6"></v-divider>
+
+      <p class="small text-black">© 2024 Government of British Columbia</p>
+    </v-container>
   </v-footer>
 </template>
 
@@ -23,13 +37,22 @@ export default defineComponent({
   setup: () => {
     const links: FooterLink[] = [
       { name: "Home", path: "/" },
+      { name: "About the ECE Registry", path: "/about" },
+      { name: "Contact Us", path: "/contact-us" },
       { name: "Disclaimer", path: "/disclaimer" },
       { name: "Privacy", path: "/privacy" },
-      { name: "Terms of Use", path: "/terms-of-use" },
       { name: "Accessibility", path: "/accessibility" },
-      { name: "Contact Us", path: "/contact-us" },
+      { name: "Copyright", path: "/copyright" },
     ];
     return { links };
+  },
+  computed: {
+    firstColumnLinks() {
+      return this.links.slice(0, Math.floor(this.links.length / 2));
+    },
+    secondColumnLinks() {
+      return this.links.slice(Math.floor(this.links.length / 2));
+    },
   },
 });
 </script>
