0ff902b78de447d4d9807452ea20ded13f36681a
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-05-27 21:23:16 -0700

    UI for invalid page and Banner component

---
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/reference/Invalid.vue
@@ -0,0 +1,14 @@
+<template>
+  <Banner type="error" title="Sorry, this link is no longer valid" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import Banner from "@/components/Banner.vue";
+
+export default defineComponent({
+  name: "Invalid",
+  components: { Banner },
+});
+</script>
