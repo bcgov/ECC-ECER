c75f5365ace4fc593ec2bc0c74c1d08da17fe989
soletsdev <soletsdev@github>
2024-05-28 14:42:19 -0700

    fixed it so we are passing in applicationId

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Submitted.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Submitted.vue
@@ -27,7 +27,7 @@
           </ul>
         </div>
       </v-row>
-      <router-link :to="'/manage-application'">
+      <router-link :to="{ name: 'manageApplication', params: { applicationId: applicationId } }">
         <v-btn class="mt-5" type="" rounded="lg" color="primary">Go to application summary</v-btn>
       </router-link>
     </v-col>
@@ -41,6 +41,11 @@ import PageContainer from "@/components/PageContainer.vue";
 export default defineComponent({
   name: "Submitted",
   components: { PageContainer },
-  props: {},
+  props: {
+    applicationId: {
+      type: String,
+      required: true,
+    },
+  },
 });
 </script>
