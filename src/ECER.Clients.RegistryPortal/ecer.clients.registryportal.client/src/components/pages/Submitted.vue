5c7f51826b3788d1bc51ceee2a47b5381f8c4a47
soletsdev <soletsdev@github>
2024-05-28 13:36:20 -0700

    changing route to point to manage application after submission

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Submitted.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Submitted.vue
@@ -27,8 +27,8 @@
           </ul>
         </div>
       </v-row>
-      <router-link :to="'/'">
-        <v-btn class="mt-5" type="" rounded="lg" color="primary">Go to Dashboard</v-btn>
+      <router-link :to="'/manage-application'">
+        <v-btn class="mt-5" type="" rounded="lg" color="primary">Go to application summary</v-btn>
       </router-link>
     </v-col>
   </PageContainer>
