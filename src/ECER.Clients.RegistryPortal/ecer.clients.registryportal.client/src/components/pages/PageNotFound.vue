5d106bde14ce75cb1ac203db1222369857048e14
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-06-03 12:34:18 -0700

    Copy changes

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/PageNotFound.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/PageNotFound.vue
@@ -7,36 +7,16 @@
         <h3>We suggest</h3>
         <ul class="ml-10">
           <li>Check you’ve entered the correct URL</li>
-          <li>
-            Go to our
-            <router-link to="/">Home page</router-link>
-            and browse through our topics for the information you want or using the search function at the top of
-            <router-link to="/">Home page</router-link>
-          </li>
           <li>Clear your browsers cache of temporary files and cookies</li>
           <li>Update your bookmarks</li>
-        </ul>
-      </div>
-      <div class="d-flex flex-column ga-3">
-        <h3>Or, contact the ECE Registry</h3>
-        <ul class="ml-10">
           <li>
-            Email:
-            <a href="mailto:ECERegistry@gov.bc.ca">ECERegistry@gov.bc.ca</a>
+            If needed,
+            <a href="https://www2.gov.bc.ca/gov/content?id=9376DE7539D44C64B3E667DB53320E71">contact the ECE Registry</a>
           </li>
-          <li>
-            Victoria:
-            <a href="tel:2503876121">250 387-6121</a>
-          </li>
-          <li>
-            Toll-free within Canada and USA:
-            <a href="tel:18006637867">1 800 663-7867</a>
-          </li>
-          <li>Outside of Canada/USA: ?</li>
         </ul>
       </div>
     </div>
-    <v-btn class="mt-10" color="primary" @click="$router.push('/')">Go to home page</v-btn>
+    <v-btn class="mt-16" color="primary" @click="$router.push('/')">Go to home page</v-btn>
   </v-container>
 </template>
 
