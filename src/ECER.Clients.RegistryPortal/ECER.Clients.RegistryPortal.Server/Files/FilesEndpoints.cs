68f1b35d8a00129f75d54d64000ce1ce702623fc
Farzad nadiri <farzadnadiri7@gmail.com>
2024-06-27 12:55:58 -0700

    ECER-2387: File Upload endpoint created

---
+++ src/ECER.Clients.RegistryPortal/ECER.Clients.RegistryPortal.Server/Files/FilesEndpoints.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using ECER.Managers.Admin.Contract.Files;
+using ECER.Utilities.Hosting;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECER.Clients.RegistryPortal.Server.Files;
+
+public class FilesEndpoints : IRegisterEndpoints
+{
+  public void Register(IEndpointRouteBuilder endpointRouteBuilder)
+  {
+    endpointRouteBuilder.MapPost("/api/files/{fileId}", async Task<Results<Ok<UploadFileResponse>, BadRequest<ProblemDetails>, NotFound>> (string fileId,
+        [FromHeader(Name = "file-classification")][Required] string classification,
+        [FromHeader(Name = "file-tag")] string? tags,
+        [FromForm(Name = "file")] IFormFile file, HttpContext httpContext, CancellationToken ct, IMediator messageBus, IMapper mapper) =>
+    {
+      if (!Guid.TryParse(fileId, out _))
+      {
+        return TypedResults.BadRequest(new ProblemDetails { Title = "Invalid GUID" });
+      }
+
+      if (file == null || file.Length == 0)
+      {
+        return TypedResults.BadRequest(new ProblemDetails { Title = "No file uploaded or file is empty" });
+      }
+      var fileProperties = new FileProperties() { Classification = classification, Tags = tags };
+      var folder = "tempfolder";
+      var files = httpContext.Request.Form.Files.Select(file => new FileData(new FileLocation(fileId, folder ?? string.Empty), fileProperties, file.FileName, file.ContentType, file.OpenReadStream())).ToList();
+      if (files.Count == 0) return TypedResults.BadRequest(new ProblemDetails { Title = "No files were uploaded" });
+      await messageBus.Send(new SaveFileCommand(files), ct);
+      return TypedResults.Ok(new UploadFileResponse(string.Empty));
+    })
+      .WithOpenApi("Handles upload file request", string.Empty, "upload_file")
+      .RequireAuthorization()
+      .DisableAntiforgery();
+  }
+}
+
+/// <summary>
+/// upload file Response
+/// </summary>
+/// <param name="fileId"></param>
+public record UploadFileResponse(string fileId);
---
+++ src/ECER.Tests/Integration/RegistryApi/FileTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Net.Http.Headers;
+using Alba;
+using Bogus;
+using Shouldly;
+using Xunit.Abstractions;
+using Xunit.Categories;
+
+namespace ECER.Tests.Integration.RegistryApi;
+
+public class FileTests : RegistryPortalWebAppScenarioBase
+{
+  private readonly Faker faker = new Faker("en_CA");
+
+  public FileTests(ITestOutputHelper output, RegistryPortalWebAppFixture fixture) : base(output, fixture)
+  { }
+
+  [Fact]
+  [Category("VPN")]
+  public async Task CanUploadFile()
+  {
+    var fileLength = 1041;
+    var testFile = await faker.GenerateTestFile(fileLength);
+    var testFileId = Guid.NewGuid().ToString();
+    var testFolder = "integrationtests";
+    var testTags = "tag1=1,tag2=2";
+    var testClassification = "test-classification";
+    using var content = new StreamContent(testFile.Content);
+    content.Headers.ContentType = new MediaTypeHeaderValue(testFile.ContentType);
+
+    using var formData = new MultipartFormDataContent
+        {
+          { content, "file", testFile.FileName }
+        };
+
+    await Host.Scenario(_ =>
+    {
+      _.WithExistingUser(this.Fixture.AuthenticatedBcscUserIdentity, this.Fixture.AuthenticatedBcscUserId);
+      _.WithRequestHeader("file-classification", testClassification);
+      _.WithRequestHeader("file-tag", testTags);
+      _.WithRequestHeader("file-folder", testFolder);
+      _.Post.MultipartFormData(formData).ToUrl($"/api/files/{testFileId}");
+      _.StatusCodeShouldBeOk();
+    });
+
+    var response = await Host.Scenario(_ =>
+    {
+      _.WithRequestHeader("file-folder", testFolder);
+      _.Get.Url($"/api/files/{testFileId}");
+      _.StatusCodeShouldBeOk();
+    });
+
+    response.Context.Response.Headers["Content-Type"].ToString().ShouldBe(testFile.ContentType);
+    response.Context.Response.Headers["Content-Disposition"].ToString().Split(';').Select(s => s.Trim()).ShouldContain($"filename={testFile.FileName}");
+    response.Context.Response.Headers["file-folder"].ToString().ShouldBe(testFolder);
+    response.Context.Response.Headers["file-tag"].ToString().ShouldBe(testTags);
+    response.Context.Response.Headers["file-classification"].ToString().ShouldBe(testClassification);
+
+    var returnedFile = await response.ReadAsTextAsync();
+    returnedFile.Length.ShouldBe(fileLength);
+    testFile.Content.Position = 0;
+    using var sw = new StreamReader(testFile.Content);
+    returnedFile.ShouldBe(await sw.ReadToEndAsync());
+  }
+}
