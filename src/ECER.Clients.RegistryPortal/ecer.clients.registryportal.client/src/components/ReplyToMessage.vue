3bdca836fd244177fd02a24b7b74b48101f233c0
Farzad nadiri <farzadnadiri7@gmail.com>
2024-07-02 13:48:37 -0700

    ECER-2390: File Upload layout

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ReplyToMessage.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ReplyToMessage.vue
@@ -3,7 +3,6 @@
     <v-row>
       <v-col cols="12">
         <v-btn prepend-icon="mdi-close" variant="text" text="Close" @click="showCloseDialog = true"></v-btn>
-
         <hr class="w-full" />
         <h1 class="mt-5">{{ messageStore.currentMessage?.subject }}</h1>
         <v-row class="mt-5">
@@ -23,6 +22,21 @@
           </v-col>
         </v-row>
         <v-row>
+          <v-col>
+            <p>
+              <v-icon size="large" icon="mdi-attachment" />
+              Attachments
+            </p>
+            <p>You can only upload PDF files up to 10MB.</p>
+            <v-btn prepend-icon="mdi-plus" variant="text" color="primary" class="mt-3" @click="triggerFileInput">Add file</v-btn>
+            <input ref="fileInput" type="file" style="display: none" accept="application/pdf" @change="handleFileUpload" />
+
+            <v-list lines="two" class="flex-grow-1 message-list">
+              <UploadFileItem v-for="(file, index) in selectedFiles" :key="index" :file="file" />
+            </v-list>
+          </v-col>
+        </v-row>
+        <v-row class="mt-10">
           <v-col>
             <v-btn size="large" color="primary" :loading="loadingStore.isLoading('message_post')" @click="handleReplyToMessage">Send</v-btn>
           </v-col>
@@ -45,12 +59,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
 
 import { sendMessage } from "@/api/message";
 import ConfirmationDialog from "@/components/ConfirmationDialog.vue";
 import PageContainer from "@/components/PageContainer.vue";
+import UploadFileItem from "@/components/UploadFileItem.vue";
 import { useAlertStore } from "@/store/alert";
 import { useLoadingStore } from "@/store/loading";
 import { useMessageStore } from "@/store/message";
@@ -58,13 +73,35 @@ import * as Rules from "@/utils/formRules";
 
 export default defineComponent({
   name: "ReplyToMessage",
-  components: { PageContainer, ConfirmationDialog },
+  components: { PageContainer, ConfirmationDialog, UploadFileItem },
   setup() {
     const messageStore = useMessageStore();
     const loadingStore = useLoadingStore();
     const alertStore = useAlertStore();
     const router = useRouter();
-    return { messageStore, loadingStore, alertStore, router };
+
+    const fileInput = ref<HTMLInputElement | null>(null);
+    const selectedFiles = ref<File[]>([]);
+
+    const triggerFileInput = () => {
+      fileInput.value?.click();
+    };
+
+    const handleFileUpload = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      const file = target.files?.[0];
+      if (file) {
+        if (file.size > 10 * 1024 * 1024) {
+          // 10MB in bytes
+          alertStore.setFailureAlert("File size exceeds the 10MB limit.");
+          return;
+        }
+        selectedFiles.value.push(file);
+        console.log("File uploaded:", file);
+      }
+    };
+
+    return { messageStore, loadingStore, alertStore, router, fileInput, triggerFileInput, handleFileUpload, selectedFiles };
   },
   data() {
     return {
@@ -80,7 +117,7 @@ export default defineComponent({
         if (error) {
           this.alertStore.setFailureAlert("Sorry, something went wrong and your changes could not be saved. Try again later.");
         } else {
-          this.alertStore.setSuccessAlert("Message sent sucessfully.");
+          this.alertStore.setSuccessAlert("Message sent successfully.");
           this.router.push("/messages");
         }
       } else {
---
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/UploadFileItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-list-item>
+    <v-row class="d-flex align-center">
+      <!-- File Name and Size -->
+      <v-col cols="4">
+        <div>{{ file.name }} ({{ (file.size / 1024).toFixed(2) }} KB)</div>
+      </v-col>
+
+      <!-- Progress Bar or Upload Completed -->
+      <v-col cols="4">
+        <div v-if="isUploadComplete">Upload completed</div>
+        <v-progress-linear v-else :value="uploadProgress" height="20" color="primary"></v-progress-linear>
+      </v-col>
+
+      <!-- Delete Button -->
+      <v-col cols="4" class="d-flex justify-end">
+        <v-icon @click="deleteFile">mdi-delete</v-icon>
+      </v-col>
+    </v-row>
+  </v-list-item>
+  <v-divider></v-divider>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UploadFileItem",
+  props: {
+    file: {
+      type: File,
+      required: true,
+    },
+    uploadProgress: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const isUploadComplete = computed(() => props.uploadProgress >= 100);
+
+    return {
+      isUploadComplete,
+    };
+  },
+  methods: {
+    deleteFile() {
+      this.$emit("delete-file", this.file);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
