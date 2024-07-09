e6026674141083022b2dcb393fbda61d28a6cc0a
Farzad nadiri <farzadnadiri7@gmail.com>
2024-07-09 14:17:36 -0700

    ECER-2486: Certification Manager Layer

---
+++ src/ECER.Managers.Registry.Contract/Certifications/Contract.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+
+namespace ECER.Managers.Registry.Contract.Certifications;
+
+public record UserCertificationQuery : IRequest<CertificationsQueryResults>
+{
+  public string? ById { get; set; }
+  public string? ByApplicantId { get; set; }
+}
+public record CertificationsQueryResults(IEnumerable<Certification> Items);
+
+public record Certification(string Id)
+{
+  public string? Number { get; set; }
+  public DateTime? ExpiryDate { get; set; }
+  public DateTime? EffectiveDate { get; set; }
+  public DateTime? Date { get; set; }
+  public bool? HasConditions { get; set; }
+  public bool? Level { get; set; }
+  public CertificateStatusCode? StatusCode { get; set; }
+  public YesNoNull? IneligibleReference { get; set; }
+}
+
+public enum CertificateStatusCode
+{
+  Active,
+  Cancelled,
+  Expired,
+  Inactive,
+  Reprinted,
+  Suspended
+}
+
+public enum YesNoNull
+{
+  No,
+  Yes,
+}
---
+++ src/ECER.Managers.Registry/CertificationHandlers.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ECER.Infrastructure.Common;
+using ECER.Managers.Registry.Contract.Certifications;
+using ECER.Resources.Documents.Certifications;
+using MediatR;
+
+namespace ECER.Managers.Registry;
+
+public class CertificationHandlers(ICertificationRepository CertificationRepository, IMapper mapper)
+    : IRequestHandler<Contract.Certifications.UserCertificationQuery, CertificationsQueryResults>
+
+{
+  public async Task<CertificationsQueryResults> Handle(Contract.Certifications.UserCertificationQuery request, CancellationToken cancellationToken)
+  {
+    ArgumentNullException.ThrowIfNull(CertificationRepository);
+    ArgumentNullException.ThrowIfNull(mapper);
+    ArgumentNullException.ThrowIfNull(request);
+
+    var Certifications = await CertificationRepository.Query(new Resources.Documents.Certifications.UserCertificationQuery
+    {
+      ById = request.ById,
+      ByApplicantId = request.ByApplicantId,
+    });
+
+    return new CertificationsQueryResults(mapper.Map<IEnumerable<Contract.Certifications.Certification>>(Certifications)!);
+  }
+}
