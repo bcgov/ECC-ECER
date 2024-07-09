c2811d8c68cb24eefbf081e0308a48fc4dc72c3c
Farzad nadiri <farzadnadiri7@gmail.com>
2024-07-09 14:16:46 -0700

    ECER-2486: Certification Repository Layer

---
+++ src/ECER.Resources.Documents/Certifications/CertificationRepository.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using ECER.Utilities.DataverseSdk.Model;
+
+namespace ECER.Resources.Documents.Certifications;
+
+internal class CertificationRepository : ICertificationRepository
+{
+  private readonly EcerContext context;
+  private readonly IMapper mapper;
+
+  public CertificationRepository(EcerContext context, IMapper mapper)
+  {
+    this.context = context;
+    this.mapper = mapper;
+  }
+
+  public async Task<IEnumerable<Certification>> Query(UserCertificationQuery query)
+  {
+    await Task.CompletedTask;
+    var Certifications = from c in context.ecer_CertificateSet
+                         select c;
+
+    // Filtering by ID
+    if (query.ById != null) Certifications = Certifications.Where(r => r.ecer_CertificateId == Guid.Parse(query.ById));
+
+    // Filtering by applicant ID
+    if (query.ByApplicantId != null) Certifications = Certifications.Where(r => r.ecer_Registrantid.Id == Guid.Parse(query.ByApplicantId));
+
+    Certifications = Certifications.OrderByDescending(r => r.ecer_ExpiryDate);
+    return mapper.Map<IEnumerable<Certification>>(Certifications.ToList());
+  }
+}
---
+++ src/ECER.Resources.Documents/Certifications/CertificationRepositoryMapper.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using AutoMapper.Extensions.EnumMapping;
+using ECER.Utilities.DataverseSdk.Model;
+
+namespace ECER.Resources.Documents.Certifications;
+
+internal class CertificationRepositoryMapper : Profile
+{
+  public CertificationRepositoryMapper()
+  {
+    CreateMap<ecer_Certificate, Certification>(MemberList.Destination)
+     .ForMember(d => d.Id, opts => opts.MapFrom(s => s.ecer_CertificateId))
+     .ForMember(d => d.Number, opts => opts.MapFrom(s => s.ecer_CertificateNumber))
+     .ForMember(d => d.ExpiryDate, opts => opts.MapFrom(s => s.ecer_ExpiryDate))
+     .ForMember(d => d.EffectiveDate, opts => opts.MapFrom(s => s.ecer_EffectiveDate))
+     .ForMember(d => d.Date, opts => opts.MapFrom(s => s.ecer_Date))
+     .ForMember(d => d.HasConditions, opts => opts.MapFrom(s => s.ecer_HasConditions))
+     .ForMember(d => d.Level, opts => opts.MapFrom(s => s.ecer_CertificateLevel))
+     .ForMember(d => d.StatusCode, opts => opts.MapFrom(s => s.StatusCode))
+     .ForMember(d => d.IneligibleReference, opts => opts.MapFrom(s => s.ecer_IneligibleReference));
+
+    CreateMap<CertificateStatusCode, ecer_certifications_StatusCode>()
+    .ConvertUsingEnumMapping(opts => opts.MapByName(true))
+    .ReverseMap();
+
+    CreateMap<YesNoNull, ecer_YesNoNull>()
+    .ConvertUsingEnumMapping(opts => opts.MapByName(true))
+    .ReverseMap();
+  }
+}
