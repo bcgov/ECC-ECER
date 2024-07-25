3da5c2ef34d66697c47b02f73a27a9b174b2dc71
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-07-25 15:51:26 -0700

    Reorganize components

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/CertificationTypeRequirements.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/CertificationTypeRequirements.vue
@@ -1,39 +1,37 @@
 <template>
   <v-container>
     <Breadcrumb :items="items" />
-
-    <div v-for="certificationType in certificationTypes" :key="certificationType">
-      <template v-if="certificationType === CertificationType.ECE_ASSISTANT">
-        <ECEAssistantRequirements />
-      </template>
-      <template v-if="certificationType === CertificationType.ONE_YEAR">
-        <ECEOneYearRequirements />
-      </template>
-      <template v-if="certificationType === CertificationType.FIVE_YEAR">
-        <ECEFiveYearRequirements />
-      </template>
-      <template v-if="certificationType === CertificationType.SNE">
-        <SneRequirements />
-      </template>
-      <template v-if="certificationType === CertificationType.ITE">
-        <IteRequirements />
-      </template>
-    </div>
+    <template v-if="applicationStore.isDraftCertificateTypeEceAssistant">
+      <ECEAssistantRequirements v-if="!applicationStore.isDraftApplicationRenewal" />
+      <ECEAssistantRenewalRequirements v-else />
+    </template>
+    <template v-if="applicationStore.isDraftCertificateTypeOneYear">
+      <ECEOneYearRequirements v-if="!applicationStore.isDraftApplicationRenewal" />
+      <ECEOneYearRenewalRequirements v-else />
+    </template>
+    <template v-if="applicationStore.isDraftCertificateTypeFiveYears">
+      <ECEFiveYearRequirements v-if="!applicationStore.isDraftApplicationRenewal" />
+      <ECEFiveYearRenewalRequirements v-else />
+      <SneRequirements v-if="applicationStore.isDraftCertificateTypeSne" />
+      <IteRequirements v-if="applicationStore.isDraftCertificateTypeIte" />
+    </template>
     <v-btn class="mt-6" rounded="lg" color="primary" @click="continueClick">Continue</v-btn>
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, type PropType } from "vue";
+import { defineComponent } from "vue";
 
 import Breadcrumb from "@/components/Breadcrumb.vue";
+import ECEAssistantRenewalRequirements from "@/components/ECEAssistantRenewalRequirements.vue";
 import ECEAssistantRequirements from "@/components/ECEAssistantRequirements.vue";
+import ECEFiveYearRenewalRequirements from "@/components/ECEFiveYearRenewalRequirements.vue";
 import ECEFiveYearRequirements from "@/components/ECEFiveYearRequirements.vue";
+import ECEOneYearRenewalRequirements from "@/components/ECEOneYearRenewalRequirements.vue";
 import ECEOneYearRequirements from "@/components/ECEOneYearRequirements.vue";
 import IteRequirements from "@/components/IteRequirements.vue";
 import SneRequirements from "@/components/SneRequirements.vue";
-import type { Components } from "@/types/openapi";
-import { CertificationType } from "@/utils/constant";
+import { useApplicationStore } from "@/store/application";
 
 export default defineComponent({
   name: "CertificationTypeRequirements",
@@ -43,17 +41,16 @@ export default defineComponent({
     ECEFiveYearRequirements,
     SneRequirements,
     IteRequirements,
+    ECEAssistantRenewalRequirements,
+    ECEOneYearRenewalRequirements,
+    ECEFiveYearRenewalRequirements,
     Breadcrumb,
-  },
-  props: {
-    certificationTypes: {
-      type: Array as PropType<Components.Schemas.CertificationType[]>,
-      required: true,
-    },
   },
 
   setup: () => {
-    return { CertificationType };
+    const applicationStore = useApplicationStore();
+
+    return { applicationStore };
   },
   data() {
     return {
