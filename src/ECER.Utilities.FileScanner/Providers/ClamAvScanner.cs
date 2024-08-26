bdaa75d011b92ed7102a7150db55070c30d500c8
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-08-26 09:45:23 -0700

    ECER-2657: fixed clamav health check dependency

--- src/ECER.Utilities.FileScanner/Providers/ClamAvScanner.cs
+++ src/ECER.Utilities.FileScanner/Providers/ClamAvScanner.cs
@@ -2,32 +2,18 @@
 
 namespace ECER.Utilities.FileScanner.Providers;
 
-public class ClamAvScanner : IFileScannerProvider
+public class ClamAvScanner(IClamClient clamClient) : IFileScannerProvider
 {
-  private readonly ClamClient _clamClient;
-
-  public ClamAvScanner(ClamClient clamClient)
-  {
-    _clamClient = clamClient ?? throw new ArgumentNullException(nameof(clamClient));
-  }
-
   public async Task<ScanResult> ScanAsync(Stream stream, CancellationToken ct = default)
   {
-    try
-    {
-      var scanResult = await _clamClient.SendAndScanFileAsync(stream, ct);
+    var scanResult = await clamClient.SendAndScanFileAsync(stream, ct);
 
-      return scanResult.Result switch
-      {
-        ClamScanResults.Clean => new ScanResult(true, "File is clean."),
-        ClamScanResults.VirusDetected => new ScanResult(false, $"Virus detected: {scanResult.InfectedFiles?[0].VirusName}"),
-        ClamScanResults.Error => new ScanResult(false, $"Error during scan: {scanResult.RawResult}"),
-        _ => new ScanResult(false, "Unknown result from ClamAV.")
-      };
-    }
-    catch (Exception ex)
+    return scanResult.Result switch
     {
-      return new ScanResult(false, $"Exception occurred: {ex.Message}");
-    }
+      ClamScanResults.Clean => new ScanResult(true, "File is clean."),
+      ClamScanResults.VirusDetected => new ScanResult(false, $"Virus detected: {scanResult.InfectedFiles?[0].VirusName}"),
+      ClamScanResults.Error => new ScanResult(false, $"Error during scan: {scanResult.RawResult}"),
+      _ => throw new NotImplementedException($"Result {scanResult.Result}")
+    };
   }
 }
