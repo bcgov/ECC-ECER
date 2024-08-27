c58ef88f177ea8fe11a05fb63a518ab60333be4b
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-08-27 13:51:52 -0700

    added ability to configure dataverse connection more granularly

---
+++ src/ECER.Infrastructure.Common/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Security;
+
+namespace ECER.Infrastructure.Common;
+
+public static class StringExtensions
+{
+  /// <summary>
+  /// Converts an unsecure string to a <see cref="SecureString"/>.
+  /// </summary>
+  /// <param name="unsecureString">The unsecure string to operate on</param>
+  /// <returns><see cref="SecureString"/></returns>
+  public static SecureString? ToSecureString(this string? unsecureString)
+  {
+    if (unsecureString == null) return null;
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+    return unsecureString.Aggregate(
+        new SecureString(),
+        (s, c) =>
+        {
+          s.AppendChar(c);
+          return s;
+        },
+        (s) =>
+        {
+          s.MakeReadOnly();
+          return s;
+        });
+#pragma warning restore CA2000 // Dispose objects before losing scope
+  }
+}
--- src/ECER.Utilities.DataverseSdk/Configurer.cs
+++ src/ECER.Utilities.DataverseSdk/Configurer.cs
@@ -3,8 +3,9 @@ using ECER.Utilities.DataverseSdk.Model;
 using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.PowerPlatform.Dataverse.Client.Model;
 using System.Diagnostics.CodeAnalysis;
 
 namespace ECER.Utilities.DataverseSdk;
@@ -13,13 +14,31 @@ public class Configurer : IConfigureComponents, IPostConfigureChecker
 {
   public void Configure([NotNull] ConfigurationContext configurationContext)
   {
+    configurationContext.Services.AddOptions<DataverseSettings>().Bind(configurationContext.Configuration.GetSection("Dataverse"));
     configurationContext.Services.AddSingleton<IOrganizationServiceAsync>(sp =>
     {
-      var logger = sp.GetRequiredService<ILogger<ServiceClient>>();
-      var client = new ServiceClient(configurationContext.Configuration.GetSection("Dataverse").GetValue("ConnectionString", string.Empty), logger);
+      var settings = sp.GetRequiredService<IOptions<DataverseSettings>>().Value;
+      ServiceClient client;
+      if (!string.IsNullOrWhiteSpace(settings.ConnectionString))
+
+      {
+        client = new ServiceClient(configurationContext.Configuration.GetSection("Dataverse").GetValue("ConnectionString", string.Empty));
+      }
+      else if (settings.ConnectionOptions != null)
+      {
+        settings.ConnectionOptions.Password = sp.GetRequiredService<IConfiguration>().GetSection("Dataverse:ConnectionOptions").GetValue<string>("Password").ToSecureString();
+        client = new ServiceClient(settings.ConnectionOptions, true, settings.ConfigurationOptions);
+        client.Connect();
+      }
+      else
+      {
+        throw new InvalidOperationException($"Dataverse is not configured properly");
+      }
+
       if (!client.IsReady) throw new InvalidOperationException($"Failed to connect to Dataverse: {client.LastError}", client.LastException);
       return client;
     });
+
     configurationContext.Services.AddScoped(sp =>
     {
       var client = sp.GetRequiredService<IOrganizationServiceAsync>();
@@ -32,4 +51,11 @@ public class Configurer : IConfigureComponents, IPostConfigureChecker
     var ctx = context.Services.GetRequiredService<IOrganizationServiceAsync>();
     await ctx.ExecuteAsync(new WhoAmIRequest());
   }
+}
+
+public record DataverseSettings
+{
+  public string? ConnectionString { get; set; }
+  public ConnectionOptions? ConnectionOptions { get; set; }
+  public ConfigurationOptions? ConfigurationOptions { get; set; }
 }
