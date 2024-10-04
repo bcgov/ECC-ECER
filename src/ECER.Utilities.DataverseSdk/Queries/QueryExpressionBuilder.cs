cd88021faac8ec24ada66c9fa2095bfb7b3bd51a
Farzad nadiri <farzadnadiri7@gmail.com>
2024-10-04 11:09:22 -0700

    merge problem fixed

--- src/ECER.Utilities.DataverseSdk/Queries/QueryExpressionBuilder.cs
+++ src/ECER.Utilities.DataverseSdk/Queries/QueryExpressionBuilder.cs
@@ -47,10 +47,11 @@ public class QueryExpressionBuilder<TEntity>
 
   public Join2QueryExpressionBuilder<TEntity> Join() => new(this);
 
-
   protected virtual EntityCollection RetreiveEntities() =>
    ((RetrieveMultipleResponse)Context.Execute(new RetrieveMultipleRequest { Query = Query })).EntityCollection;
 
+  protected static EntityCollection RetreiveEntities([NotNull] OrganizationServiceContext context, [NotNull] QueryExpression query) =>
+  ((RetrieveMultipleResponse)context.Execute(new RetrieveMultipleRequest { Query = query })).EntityCollection;
 
   protected virtual void AttachEntities([NotNull] IEnumerable<Entity> entities)
   {
