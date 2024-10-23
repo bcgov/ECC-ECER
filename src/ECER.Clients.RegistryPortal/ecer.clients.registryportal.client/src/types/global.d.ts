eba1fe911a80c90df2be0a86a3e9ad31be48f963
soletsdev <soletsdev@github>
2024-10-23 15:20:17 -0700

    fix recaptcha error when sending public lookup twice

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/types/global.d.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/types/global.d.ts
@@ -2,7 +2,7 @@ declare global {
   interface Window {
     grecaptcha: {
       getResponse(): string;
-      reset(string): void;
+      reset(string?): void; //if widget id is not provided, will default to first recaptcha instance
       render(string): string;
     };
     recaptchaSuccessCallback: function;
