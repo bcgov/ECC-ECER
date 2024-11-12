593ec283fa233e274b830faf79d1943e175a3cc4
Farzad nadiri <farzadnadiri7@gmail.com>
2024-11-12 13:55:02 -0800

    ECER-3671: Certification Tests added to project

---
+++ src/ECER.Tests/Integration/Api/CertificationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace ECER.Tests.Integration.Api;
+
+public class CertificationTests : ApiWebAppScenarioBase
+{
+  public CertificationTests(ITestOutputHelper output, ApiWebAppFixture fixture) : base(output, fixture)
+  {
+  }
+}
