1ad38d95f27cff5aa370849b219d20ecb561af2b
Parker Atkins <parker.cattell.atkins@gmail.com>
2024-11-18 15:16:56 -0800

    ECER-3584

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ECEOneYearContent.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ECEOneYearContent.vue
@@ -3,11 +3,16 @@
     <v-row>
       <v-col cols="12">
         <p>
-          This certification allows you to work as a fully certified ECE while you work towards your 500 hours work experience needed to apply for a ECE Five
-          Year certification.
+          This certification allows you to work alone and/or as the primary educator in licensed child care programs for children 3 to 5 years of age and to
+          work alongside an Infant Toddler Educator in licensed programs for children under 36-months.
         </p>
         <br />
-        <p>You must have completed a basic early childhood education training program. You will only be able to renew this certification once.</p>
+        <p>If you have not completed 500 hours of work experience under the supervision of a Canadian certified ECE, you can apply for this certification.</p>
+        <br />
+        <p>
+          This certificate needs to be renewed within 1 year if you have not completed the 500 hours of supervised work experience to qualify for an ECE Five
+          Year Certificate and can only be renewed once.
+        </p>
       </v-col>
     </v-row>
   </v-container>
