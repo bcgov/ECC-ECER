45426a50ba4f14caade8151306822345d21cb36d
Parker Atkins <parker.cattell.atkins@gmail.com>
2025-01-26 13:43:04 -0800

    Login page adjustments

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Login.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/pages/Login.vue
@@ -2,25 +2,26 @@
   <v-sheet :rounded="0" flat color="primary" width="100%">
     <PageContainer :margin-top="false" class="py-13">
       <h1 class="text-white">Welcome to the Early Childhood Educator (ECE) Registry</h1>
-      <p class="small text-white mt-4">Apply or manage your certification to work in a child care facility in British Columbia.</p>
+      <p class="small text-white mt-4">Manage your certification to work in a licensed child care facility in B.C.</p>
     </PageContainer>
   </v-sheet>
   <PageContainer :margin-top="false">
-    <div class="d-flex flex-column ga-3">
-      <h2>Log in with BC Services Card</h2>
-      <p>A BC Services Card account is:</p>
-      <ul class="ml-10">
-        <li>A secure login service provided by the government of British Columbia</li>
-        <li>An account you can use to log in to My ECE Registry and many other government services</li>
-        <li>Available to people with ID issued in Canada</li>
-      </ul>
-    </div>
-    <v-btn @click="handleLogin('bcsc')" class="my-8" :size="smAndDown ? 'default' : 'large'" color="primary" append-icon="mdi-arrow-right">
-      Log in with BC Services Card
-    </v-btn>
-    <p>
-      <a href="https://id.gov.bc.ca/account/setup-instruction" target="_blank">How to set up a BC Services Card account</a>
-    </p>
+    <v-row>
+      <v-col cols="12" md="6" class="mb-12">
+        <div class="d-flex flex-column ga-4">
+          <h2>First time using My ECE Registry online?</h2>
+          <p>You need to create an account to submit a new application or access your existing certifications.</p>
+        </div>
+        <v-btn class="mt-8" @click="handleCreateAccount()" :size="smAndDown ? 'default' : 'large'" color="primary">Create account</v-btn>
+      </v-col>
+      <v-col cols="12" md="6">
+        <h2>Already have an account?</h2>
+        <div class="d-inline-flex flex-column ga-8 mt-6">
+          <v-btn @click="handleLogin('bcsc')" :size="smAndDown ? 'default' : 'large'" color="primary" variant="outlined">Log in with BC Services Card</v-btn>
+          <v-btn @click="handleLogin('bceid')" :size="smAndDown ? 'default' : 'large'" color="primary" variant="outlined">Log in with Basic BCeID</v-btn>
+        </div>
+      </v-col>
+    </v-row>
   </PageContainer>
 </template>
 
@@ -51,6 +52,9 @@ export default defineComponent({
       const redirectTo = this.route.query.redirect_to as string;
 
       await this.oidcStore.login(provider == "bceid" ? "bceidbasic" : "bcsc", redirectTo);
+    },
+    handleCreateAccount() {
+      this.$router.push({ name: "register" });
     },
   },
 });
