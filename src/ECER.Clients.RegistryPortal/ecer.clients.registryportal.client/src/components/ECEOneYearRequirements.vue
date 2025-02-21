3bfd3b9cfc600fbcbeb25d1b946c9715a01a3a30
soletsdev <soletsdev@github>
2025-02-21 15:20:18 -0800

    ecer-4354 content update for requirements

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ECEOneYearRequirements.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/ECEOneYearRequirements.vue
@@ -30,8 +30,15 @@
       <p>The program must be either:</p>
       <br />
       <ul class="ml-10">
-        <li>Recognized by the ECE Registry</li>
-        <li>Deemed equivalent by the ECE Registry</li>
+        <!-- prettier-ignore-->
+        <li>
+          <a
+            target="_blank"
+            href="https://www2.gov.bc.ca/gov/content/education-training/early-learning/teach/training-and-professional-development/become-an-early-childhood-educator/recognized-ece-institutions"
+          >
+            Recognized</a> by the ECE Registry
+        </li>
+        <li>Considered equivalent by the ECE Registry</li>
       </ul>
       <br />
       <p>
