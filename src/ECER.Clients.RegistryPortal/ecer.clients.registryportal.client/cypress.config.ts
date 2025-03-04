9af2692d1a37c5b18cc0d340a41d802d25a778e8
Deepak Yadav <deepak.yadav@quartech.com>
2025-03-04 15:22:23 -0800

    config updated to use variables from cypress.env.json

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/cypress.config.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/cypress.config.ts
@@ -12,13 +12,16 @@ export default defineConfig({
   e2e: {
     baseUrl: env.BASE_URL || "http://localhost:5121",
     setupNodeEvents(on, config) {
-      // Implement any node event listeners here if needed.
+      // If a BASE_URL is provided via the environment, use it.
+      if (config.env.BASE_URL) {
+        config.baseUrl = config.env.BASE_URL;
+      }
       return config;
     },
   },
   env: {
-    BCSC_USERNAME: env.BCSC_USERNAME || "bcsc_username",
-    BCSC_PASSWORD: env.BCSC_PASSWORD || "bcsc_password",
+    BCSC_USERNAME: env.BCSC_USERNAME,
+    BCSC_PASSWORD: env.BCSC_PASSWORD,
     DEVICES: env.DEVICES || JSON.stringify(defaultDevices),
   },
 });
