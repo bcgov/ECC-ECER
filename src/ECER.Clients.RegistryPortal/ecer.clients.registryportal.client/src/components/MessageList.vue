9d51e900f36f4cb6d56a991f869acfd16352a9dc
Farzad nadiri <farzadnadiri7@gmail.com>
2025-03-19 11:54:11 -0700

    ECER-2852: First message loaded on message list fetch

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/MessageList.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/MessageList.vue
@@ -1,16 +1,24 @@
 <template>
   <v-list lines="two" class="flex-grow-1 message-list" style="padding: 0px">
-    <MessageListItem v-for="(message, index) in messages" :key="index" :message="message" @update:message-is-read="message.isRead = $event" />
+    <MessageListItem
+      v-for="(message, index) in messages"
+      :key="index"
+      :message="message"
+      ref="messageListItems"
+      refInFor
+      @update:message-is-read="message.isRead = $event"
+    />
     <v-pagination v-if="messageCount > 1" v-model="currentPage" size="small" class="mt-4" elevation="2" :length="totalPages"></v-pagination>
   </v-list>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, nextTick } from "vue";
 import { getMessages } from "@/api/message";
 import MessageListItem from "@/components/MessageListItem.vue";
 import { useMessageStore } from "@/store/message";
 import type { Components } from "@/types/openapi";
+
 const PAGE_SIZE = 10;
 
 export default defineComponent({
@@ -42,23 +50,32 @@ export default defineComponent({
       },
     },
   },
-  mounted() {
+  async mounted() {
     this.messageStore.$reset();
-    this.fetchMessages(this.page);
+    await this.fetchMessages(this.page);
   },
   methods: {
     async fetchMessages(page: number) {
-      const params = {
-        page,
-        pageSize: PAGE_SIZE,
-      };
+      const params = { page, pageSize: PAGE_SIZE };
       const response = await getMessages(params);
       this.messages = response.data?.communications || [];
       this.messageCount = response.data?.totalMessagesCount || 0;
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+
+      window.scrollTo({ top: 0, behavior: "smooth" });
+
+      // After the list is rendered, select/load the first message if available
+      await nextTick();
+      const messageListItems = this.$refs.messageListItems as Array<{ loadChildMessages: (id: string) => void }>;
+      if (this.messages.length > 0 && messageListItems) {
+        // Because we used refInFor, messageListItems is an array
+        const firstItem = messageListItems[0];
+        // Now call the child’s loadChildMessages method for the first message
+        if (firstItem && typeof firstItem.loadChildMessages === "function") {
+          if (this.messages[0]?.id) {
+            firstItem.loadChildMessages(this.messages[0].id);
+          }
+        }
+      }
     },
   },
 });
