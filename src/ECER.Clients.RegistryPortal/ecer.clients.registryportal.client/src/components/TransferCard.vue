d4b3a294c73b205d47ea045b8c6d6b9b039b81ec
Farzad nadiri <farzadnadiri7@gmail.com>
2025-04-16 10:21:30 -0700

    ECER-4619: Unnecessary comment removed from transfer card

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/TransferCard.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/TransferCard.vue
@@ -7,7 +7,6 @@
       </p>
     </v-card-item>
     <div class="d-flex flex-row justify-start ga-3 flex-wrap ma-4">
-      <!-- Application status Draft -->
       <v-card-actions>
         <v-btn class="pl-8 pr-8" size="large" variant="flat" color="warning">Transfer</v-btn>
       </v-card-actions>
