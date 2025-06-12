58499381764771f652b349deaf60af9f9a79fe99
Parker Atkins <parker.cattell.atkins@gmail.com>
2025-06-11 22:51:19 -0700

    WIP TermsAndConditions

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/certification.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/api/certification.ts
@@ -11,6 +11,14 @@ const getCertifications = async (): Promise<ApiResponse<Components.Schemas.Certi
   return apiResultHandler.execute<Components.Schemas.Certification[]>({ request: client.certification_get({ id: "" }) });
 };
 
+const getCertificationsById = async (id: string): Promise<ApiResponse<Components.Schemas.Certification[] | null>> => {
+  const client = await getClient();
+  return apiResultHandler.execute<Components.Schemas.Certification[]>({
+    request: client.certification_get({ id: id }),
+    key: "certification_get",
+  });
+};
+
 const requestCertificateFileGeneration = async (id: string): Promise<ApiResponse<any>> => {
   const client = await getClient();
   return apiResultHandler.execute<any>({ request: client.certification_requestpdf_put({ id: id }) });
