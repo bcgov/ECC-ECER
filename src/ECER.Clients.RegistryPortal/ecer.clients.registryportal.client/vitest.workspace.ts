e5e4c081f450b88185e7402f3e27bdb754630b24
DSO <derek.so@gov.bc.ca>
2025-07-18 11:50:07 -0700

    updating to storybook v9. Ran npx @storybook latest and made some compatiblity changes

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/vitest.workspace.ts
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/vitest.workspace.ts
@@ -1,30 +1,33 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
-import { defineWorkspace } from "vitest/config";
+import storybookTest from "@storybook/addon-vitest/vitest-plugin";
+import { defineConfig } from "vitest/config";
 
 const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
-export default defineWorkspace([
-  "vite.config.mts",
-  {
-    extends: "vite.config.mts",
-    plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      storybookTest({ configDir: path.join(dirname, ".storybook") }),
-    ],
-    test: {
-      name: "storybook",
-      browser: {
-        enabled: true,
-        headless: true,
-        name: "chromium",
-        provider: "playwright",
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            name: "chromium",
+            provider: "playwright",
+          },
+          setupFiles: ["SETUP_FILE"],
+        },
       },
-      setupFiles: [".storybook/vitest.setup.ts"],
-    },
+    ],
   },
-]);
+});
