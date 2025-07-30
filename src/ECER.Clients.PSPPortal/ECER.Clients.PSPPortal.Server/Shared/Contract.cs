6a81908fb928064a2a8cc07d57518fadbdc10082
Farzad nadiri <farzadnadiri7@gmail.com>
2025-07-30 13:02:28 -0700

    ECER-5157: Backend project built and configured, dependencies added for PSP portal

---
+++ src/ECER.Clients.PSPPortal/ECER.Clients.PSPPortal.Server/Shared/Contract.cs
@@ -0,0 +1,6 @@
+﻿namespace ECER.Clients.PSPPortal.Server.Shared;
+
+public record ClaimCacheSettings
+{
+  public double CacheTimeInSeconds { get; set; } = 500;
+}
---
+++ src/ECER.Managers.PSP/Configurer.cs
@@ -0,0 +1,13 @@
+﻿using ECER.Infrastructure.Common;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ECER.Managers.PSP;
+
+public class Configurer : IConfigureComponents
+{
+  public void Configure([NotNull] ConfigurationContext configurationContext)
+  {
+
+  }
+}
+
