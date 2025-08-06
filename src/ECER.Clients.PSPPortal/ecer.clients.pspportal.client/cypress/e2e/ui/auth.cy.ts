fb48c2727db6b79a45d09e7814b878e465804935
Farzad nadiri <farzadnadiri7@gmail.com>
2025-08-06 16:24:41 -0700

    cypress removed from frontend

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/cypress/e2e/ui/auth.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("BCSC Authentication Tests", () => {
-  it("should log in successfully", () => {
-    // Check if the profile page is available.
-    cy.visit("/profile");
-    cy.document().its("readyState").should("eq", "complete");
-    cy.contains("Email address").should("be.visible");
-  });
-});
