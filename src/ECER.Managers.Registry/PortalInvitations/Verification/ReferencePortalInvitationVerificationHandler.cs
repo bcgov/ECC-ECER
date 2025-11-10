813a7fc590b4ee8789c09df2f2b5c8414ccf7d3e
Parker Atkins <parker.cattell.atkins@gmail.com>
2025-11-10 14:29:06 -0800

    Fix ReferencePortalInvitationVerificationHandler

--- src/ECER.Managers.Registry/PortalInvitations/Verification/ReferencePortalInvitationVerificationHandler.cs
+++ src/ECER.Managers.Registry/PortalInvitations/Verification/ReferencePortalInvitationVerificationHandler.cs
@@ -3,57 +3,26 @@ using ECER.Managers.Registry.Contract.PortalInvitations;
 using ECER.Resources.Accounts.Registrants;
 using ECER.Resources.Documents.Applications;
 using ECER.Resources.Documents.Certifications;
-using ECER.Resources.Documents.PortalInvitations;
-using MediatR;
-using InviteType = ECER.Resources.Documents.PortalInvitations.InviteType;
-using PortalInvitation = ECER.Resources.Documents.PortalInvitations.PortalInvitation;
-using PortalInvitationStatusCode = ECER.Managers.Registry.Contract.PortalInvitations.PortalInvitationStatusCode;
 
 namespace ECER.Managers.Registry;
 
-public class PortalInvitationHandlers(IPortalInvitationTransformationEngine transformationEngine,
-    IPortalInvitationRepository portalInvitationRepository, IApplicationRepository applicationRepository,
-    IRegistrantRepository registrantRepository, ICertificationRepository certificationRepository, IMapper mapper)
-  : IRequestHandler<PortalInvitationVerificationQuery, PortalInvitationVerificationQueryResult>
+public class ReferencePortalInvitationVerificationHandler(
+  IRegistrantRepository registrantRepository,
+  ICertificationRepository certificationRepository,
+  IApplicationRepository applicationRepository,
+  IMapper mapper)
+  : IPortalInvitationVerificationHandler
 {
-  public async Task<PortalInvitationVerificationQueryResult> Handle(PortalInvitationVerificationQuery request,
-    CancellationToken cancellationToken)
+  public bool CanHandle(InviteType? inviteType)
   {
-    ArgumentNullException.ThrowIfNull(request);
-    var response =
-      await transformationEngine.Transform(new DecryptInviteTokenRequest(request.VerificationToken))! as
-        DecryptInviteTokenResponse ?? throw new InvalidCastException("Invalid response type");
-    if (response.PortalInvitation == Guid.Empty)
-    {
-      return PortalInvitationVerificationQueryResult.Failure("Invalid Token");
-    }
-
-    var portalInvitation =
-      await portalInvitationRepository.Query(new PortalInvitationQuery(response.PortalInvitation), cancellationToken);
-    if (portalInvitation == null)
-    {
-      return PortalInvitationVerificationQueryResult.Failure("Portal Invitation not found");
-    }
-
-    switch (portalInvitation.InviteType)
-    {
-      case InviteType.PSIProgramRepresentative:
-        return await HandlePspInvitation(portalInvitation, cancellationToken);
-      case InviteType.CharacterReference:
-      case InviteType.WorkExperienceReferenceforApplication:
-      case InviteType.WorkExperienceReferenceforICRA:
-        return await HandleReferenceInvitation(portalInvitation, cancellationToken);
-      default:
-        return PortalInvitationVerificationQueryResult.Failure("Unsupported invite type.");
-    }
+    return inviteType is InviteType.CharacterReference
+      or InviteType.WorkExperienceReferenceforApplication;
   }
 
-  private async Task<PortalInvitationVerificationQueryResult> HandleReferenceInvitation(
-    PortalInvitation portalInvitation, CancellationToken cancellationToken)
+  public async Task<PortalInvitationVerificationQueryResult> Verify(PortalInvitation portalInvitation, CancellationToken cancellationToken)
   {
     var emptyGuidString = Guid.Empty.ToString();
-    if (portalInvitation.WorkexperienceReferenceId == emptyGuidString &&
-        portalInvitation.CharacterReferenceId == emptyGuidString)
+    if (portalInvitation == null || (portalInvitation.WorkexperienceReferenceId == emptyGuidString && portalInvitation.CharacterReferenceId == emptyGuidString))
     {
       return PortalInvitationVerificationQueryResult.Failure("Reference not found");
     }
@@ -68,9 +37,7 @@ public class PortalInvitationHandlers(IPortalInvitationTransformationEngine tran
     var certifications = await certificationRepository.Query(new UserCertificationQuery() { ByApplicantId = applicant.Id });
     var latestCertification = certifications.FirstOrDefault();
 
-    var applications =
-      await applicationRepository.Query(new ApplicationQuery() { ById = portalInvitation.ApplicationId },
-        cancellationToken);
+    var applications = await applicationRepository.Query(new ApplicationQuery() { ById = portalInvitation.ApplicationId }, cancellationToken);
     var application = applications.SingleOrDefault();
     if (application == null)
     {
@@ -115,20 +82,4 @@ public class PortalInvitationHandlers(IPortalInvitationTransformationEngine tran
 
     return PortalInvitationVerificationQueryResult.Success(result);
   }
-
-  private Task<PortalInvitationVerificationQueryResult> HandlePspInvitation(PortalInvitation portalInvitation,
-    CancellationToken cancellationToken)
-  {
-    var emptyGuidString = Guid.Empty.ToString();
-    if (portalInvitation.PspProgramRepresentativeId == emptyGuidString)
-    {
-      return Task.FromResult(PortalInvitationVerificationQueryResult.Failure("Psp Program Representative not found"));
-    }
-
-    var result = mapper.Map<Contract.PortalInvitations.PortalInvitation>(portalInvitation);
-
-    return Task.FromResult(PortalInvitationVerificationQueryResult.Success(result));
-  }
 }
-
-
