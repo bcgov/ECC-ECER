ad819967357f72563d95d03214ddd6de30826a4c
DSO <derek.so@gov.bc.ca>
2025-11-17 09:15:26 -0800

    ecer-5653 quick fix variant change

--- src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/reference/Invalid.vue
+++ src/ECER.Clients.RegistryPortal/ecer.clients.registryportal.client/src/components/reference/Invalid.vue
@@ -1,5 +1,5 @@
 <template>
-  <Alert type="error" title="Sorry, this link is no longer valid" :closable="false" :prominent="true" variant="plain" :rounded="false" />
+  <Alert type="error" title="Sorry, this link is no longer valid" :closable="false" :prominent="true" variant="text" :rounded="false" />
 </template>
 
 <script lang="ts">
