09cea10c4c07e18b86a6d097daeb46625e3d0a9e
Chris Tihor <Christopher.Tihor@quartech.com>
2025-11-24 10:49:07 -0800

    Updated to trim out unneeded test scenarios.

--- src/ECER.Tests/Integration/PspApi/PortalInvitationTests.cs
+++ src/ECER.Tests/Integration/PspApi/PortalInvitationTests.cs
@@ -5,11 +5,6 @@ using ECER.Managers.Registry.Contract.PortalInvitations;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit.Abstractions;
 
 namespace ECER.Tests.Integration.PspApi;
