b5580af643e642e9606891e47f3ab0e97c94a34d
DSO <derek.so@gov.bc.ca>
2025-11-25 15:00:44 -0800

    ecer-5177 moving into namespace to fix sonar

--- src/ECER.Resources.E2ETests/UnitTest/IUnitTestRepositoryMapper.cs
+++ src/ECER.Resources.E2ETests/UnitTest/IUnitTestRepositoryMapper.cs
@@ -1,4 +1,6 @@
-﻿internal class IUnitTestRepositoryMapper
+﻿namespace ECER.Resources.E2ETests.E2ETestsContacts;
+
+internal class IUnitTestRepositoryMapper
 {
   public IUnitTestRepositoryMapper()
   {
