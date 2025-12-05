e2e7ac5fc39818b61e3ea17900ac58e299bca9ae
Parker Atkins <parker.cattell.atkins@gmail.com>
2025-12-05 11:22:49 -0800

    npm run gen-api

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/types/openapi.d.ts
+++ src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/types/openapi.d.ts
@@ -8,6 +8,48 @@ declare namespace Components {
       } | null;
     }
     export type Auspice = "ContinuingEducation" | "PublicOOP" | "Private" | "Public";
+    export interface Communication {
+      id?: string | null;
+      subject?: string | null;
+      text?: string | null;
+      from?: InitiatedFrom;
+      acknowledged?: boolean;
+      notifiedOn?: string; // date-time
+      status?: CommunicationStatus;
+      doNotReply?: boolean;
+      latestMessageNotifiedOn?: string | null; // date-time
+      isRead?: boolean | null;
+      applicationId?: string | null;
+      icraEligibilityId?: string | null;
+      programRepresentativeId?: string | null;
+      documents?: CommunicationDocument[] | null;
+    }
+    export interface CommunicationDocument {
+      id?: string | null;
+      url?: string | null;
+      extention?: string | null;
+      name?: string | null;
+      size?: string | null;
+    }
+    /**
+     * Save communication response
+     */
+    export interface CommunicationResponse {
+      /**
+       * The communication id
+       */
+      communicationId?: string | null;
+    }
+    /**
+     * Communication seen request
+     */
+    export interface CommunicationSeenRequest {
+      /**
+       * The communication ID
+       */
+      communicationId?: string | null;
+    }
+    export type CommunicationStatus = "Draft" | "NotifiedRecipient" | "Acknowledged" | "Inactive";
     export interface EducationInstitution {
       id?: string | null;
       name?: string | null;
@@ -21,6 +63,10 @@ declare namespace Components {
       country?: string | null;
       postalCode?: string | null;
     }
+    export interface GetMessagesResponse {
+      communications?: Communication[] | null;
+      totalMessagesCount?: number; // int32
+    }
     export interface HttpValidationProblemDetails {
       [name: string]: any;
       type?: string | null;
@@ -32,6 +78,7 @@ declare namespace Components {
         [name: string]: string[];
       } | null;
     }
+    export type InitiatedFrom = "Investigation" | "PortalUser" | "Registry" | "ProgramRepresentative";
     export type InviteType = "PSIProgramRepresentative";
     export interface NewPspUserResponse {
       id?: string | null;
@@ -92,6 +139,7 @@ declare namespace Components {
       phoneExtension?: string | null;
       jobTitle?: string | null;
       role?: /* Role of the PSP user */ PspUserRole;
+      unreadMessagesCount?: number; // int32
       email?: string | null;
       hasAcceptedTermsOfUse?: boolean | null;
     }
@@ -107,6 +155,21 @@ declare namespace Components {
       programRepresentativeId?: string | null;
       bceidBusinessId?: string | null;
       profile: /* User profile information */ PspUserProfile;
+    }
+    /**
+     * Send Message Request
+     */
+    export interface SendMessageRequest {
+      communication?: Communication;
+    }
+    /**
+     * Send Message Response
+     */
+    export interface SendMessageResponse {
+      /**
+       *
+       */
+      communicationId?: string | null;
     }
     export interface VersionMetadata {
       version?: string | null;
@@ -127,6 +190,32 @@ declare namespace Paths {
       export interface $404 {}
     }
   }
+  namespace GetAllMessages {
+    namespace Parameters {
+      export type ParentId = string;
+    }
+    export interface PathParameters {
+      parentId?: Parameters.ParentId;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.GetMessagesResponse;
+      export type $400 = Components.Schemas.ProblemDetails | Components.Schemas.HttpValidationProblemDetails;
+      export interface $404 {}
+    }
+  }
+  namespace MessageStatusGet {
+    namespace Parameters {
+      export type Id = string;
+    }
+    export interface PathParameters {
+      id?: Parameters.Id;
+    }
+    export type RequestBody = /* Communication seen request */ Components.Schemas.CommunicationSeenRequest;
+    namespace Responses {
+      export type $200 = /* Save communication response */ Components.Schemas.CommunicationResponse;
+      export type $400 = string;
+    }
+  }
   namespace PortalInvitationGet {
     namespace Parameters {
       export type Token = string;
@@ -137,6 +226,14 @@ declare namespace Paths {
     namespace Responses {
       export type $200 = Components.Schemas.PortalInvitationQueryResult;
       export type $400 = Components.Schemas.HttpValidationProblemDetails;
+    }
+  }
+  namespace PostMessageReply {
+    export type RequestBody = /* Send Message Request */ Components.Schemas.SendMessageRequest;
+    namespace Responses {
+      export type $200 = /* Send Message Response */ Components.Schemas.SendMessageResponse;
+      export type $400 = Components.Schemas.ProblemDetails | Components.Schemas.HttpValidationProblemDetails;
+      export interface $404 {}
     }
   }
   namespace PspUserAdd {
@@ -295,6 +392,38 @@ export interface OperationMethods {
     data?: any,
     config?: AxiosRequestConfig,
   ): OperationResponse<Paths.EducationInstitutionGet.Responses.$200>;
+  /**
+   * get_all_messages - Paginated endpoint to get all user messages
+   */
+  "get_all_messages"(
+    parameters?: Parameters<Paths.GetAllMessages.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.GetAllMessages.Responses.$200>;
+  /**
+   * post_message_reply - Endpoint to reply to an existing message
+   */
+  "post_message_reply"(
+    parameters?: Parameters<UnknownParamsObject> | null,
+    data?: Paths.PostMessageReply.RequestBody,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.PostMessageReply.Responses.$200>;
+  /**
+   * message_status_get - Handles messages status
+   */
+  "message_status_get"(
+    parameters?: Parameters<Paths.MessageStatusGet.PathParameters> | null,
+    data?: Paths.MessageStatusGet.RequestBody,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.MessageStatusGet.Responses.$200>;
+  /**
+   * message_status_get - Handles messages status
+   */
+  "message_status_get"(
+    parameters?: Parameters<Paths.MessageStatusGet.PathParameters> | null,
+    data?: Paths.MessageStatusGet.RequestBody,
+    config?: AxiosRequestConfig,
+  ): OperationResponse<Paths.MessageStatusGet.Responses.$200>;
 }
 
 export interface PathsDictionary {
@@ -402,14 +531,61 @@ export interface PathsDictionary {
       config?: AxiosRequestConfig,
     ): OperationResponse<Paths.EducationInstitutionGet.Responses.$200>;
   };
+  ["/api/messages/{parentId}"]: {
+    /**
+     * get_all_messages - Paginated endpoint to get all user messages
+     */
+    "get"(
+      parameters?: Parameters<Paths.GetAllMessages.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.GetAllMessages.Responses.$200>;
+  };
+  ["/api/messages"]: {
+    /**
+     * post_message_reply - Endpoint to reply to an existing message
+     */
+    "post"(
+      parameters?: Parameters<UnknownParamsObject> | null,
+      data?: Paths.PostMessageReply.RequestBody,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.PostMessageReply.Responses.$200>;
+  };
+  ["/api/messages/{id}/seen"]: {
+    /**
+     * message_status_get - Handles messages status
+     */
+    "put"(
+      parameters?: Parameters<Paths.MessageStatusGet.PathParameters> | null,
+      data?: Paths.MessageStatusGet.RequestBody,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.MessageStatusGet.Responses.$200>;
+  };
+  ["/api/messages/status"]: {
+    /**
+     * message_status_get - Handles messages status
+     */
+    "get"(
+      parameters?: Parameters<Paths.MessageStatusGet.PathParameters> | null,
+      data?: Paths.MessageStatusGet.RequestBody,
+      config?: AxiosRequestConfig,
+    ): OperationResponse<Paths.MessageStatusGet.Responses.$200>;
+  };
 }
 
 export type Client = OpenAPIClient<OperationMethods, PathsDictionary>;
 
 export type ApplicationConfiguration = Components.Schemas.ApplicationConfiguration;
 export type Auspice = Components.Schemas.Auspice;
+export type Communication = Components.Schemas.Communication;
+export type CommunicationDocument = Components.Schemas.CommunicationDocument;
+export type CommunicationResponse = Components.Schemas.CommunicationResponse;
+export type CommunicationSeenRequest = Components.Schemas.CommunicationSeenRequest;
+export type CommunicationStatus = Components.Schemas.CommunicationStatus;
 export type EducationInstitution = Components.Schemas.EducationInstitution;
+export type GetMessagesResponse = Components.Schemas.GetMessagesResponse;
 export type HttpValidationProblemDetails = Components.Schemas.HttpValidationProblemDetails;
+export type InitiatedFrom = Components.Schemas.InitiatedFrom;
 export type InviteType = Components.Schemas.InviteType;
 export type NewPspUserResponse = Components.Schemas.NewPspUserResponse;
 export type OidcAuthenticationSettings = Components.Schemas.OidcAuthenticationSettings;
@@ -423,4 +599,6 @@ export type PspUserListItem = Components.Schemas.PspUserListItem;
 export type PspUserProfile = Components.Schemas.PspUserProfile;
 export type PspUserRole = Components.Schemas.PspUserRole;
 export type RegisterPspUserRequest = Components.Schemas.RegisterPspUserRequest;
+export type SendMessageRequest = Components.Schemas.SendMessageRequest;
+export type SendMessageResponse = Components.Schemas.SendMessageResponse;
 export type VersionMetadata = Components.Schemas.VersionMetadata;
