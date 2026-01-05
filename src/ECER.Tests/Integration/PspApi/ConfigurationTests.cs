a1c7e86c19efc7f821c95d79c7079efe891d7296
Farzad nadiri <farzadnadiri7@gmail.com>
2026-01-05 11:05:36 -0800

    ECER-5809: Automated tests for PSP Area of Instructions

---
+++ src/ECER.Tests/Integration/PspApi/ConfigurationTests.cs
@@ -0,0 +1,39 @@
+using Alba;
+using ECER.Clients.PSPPortal.Server;
+using Shouldly;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ECER.Tests.Integration.PspApi;
+
+public class ConfigurationTests : PspPortalWebAppScenarioBase
+{
+  public ConfigurationTests(ITestOutputHelper output, PspPortalWebAppFixture fixture) : base(output, fixture)
+  {
+  }
+
+  [Fact]
+  public async Task GetAreaOfInstructions_ReturnsAreaOfInstructions()
+  {
+    var response = await Host.Scenario(_ =>
+    {
+      _.WithPspUser(Fixture.AuthenticatedPspUserIdentity, Fixture.AuthenticatedPspUserId, true);
+      _.Get.Url("/api/areaofinstructionlist");
+      _.StatusCodeShouldBeOk();
+    });
+
+    var payload = await response.ReadAsJsonAsync<AreaOfInstructionListResponse>();
+    payload.ShouldNotBeNull();
+    payload.AreaOfInstruction.ShouldNotBeNull();
+    payload.AreaOfInstruction.ShouldContain(item => item.Id == Fixture.AreaOfInstructionId);
+
+    var instruction = payload.AreaOfInstruction.Single(item => item.Id == Fixture.AreaOfInstructionId);
+    instruction.Name.ShouldBe(Fixture.AreaOfInstructionName);
+    instruction.MinimumHours.ShouldBe(Fixture.AreaOfInstructionMinimumHours);
+    instruction.ProgramTypes.ShouldNotBeNull();
+    instruction.ProgramTypes.ShouldContain("ITE");
+    instruction.ProgramTypes.ShouldContain("SNE");
+  }
+}
