fb7549b4363cc7d27d7cbce8935c0cceace7e686
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-01-06 11:03:02 -0800

    UT fix

--- src/ECER.Resources.Documents/Programs/ProgramRepositoryMapper.cs
+++ src/ECER.Resources.Documents/Programs/ProgramRepositoryMapper.cs
@@ -55,7 +55,7 @@ internal class ProgramRepositoryMapper : Profile
     CreateMap<ecer_Course, Course>(MemberList.Destination)
       .ForMember(d => d.CourseNumber, opts => opts.MapFrom(s => s.ecer_Code))
       .ForMember(d => d.CourseTitle, opts => opts.MapFrom(s => s.ecer_CourseName))
-      .ForMember(d => d.AreaOfInstructions, opts => opts.MapFrom(s => s.ecer_ProgramAreas))
+      .ForMember(d => d.AreaOfInstructions, opts => opts.MapFrom(s => s.ecer_courseprovincialrequirement_CourseId))
       .ForMember(d => d.ProgramType, opts => opts.MapFrom(s => s.ecer_ProgramType));
     
     CreateMap<ecer_CourseProvincialRequirement, AreaOfInstruction>(MemberList.Destination)
