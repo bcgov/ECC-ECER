52ba1cad2e447b21f87fb4f1a84a7b811fc84efa
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-01-21 14:06:04 -0800

    Update ProgramRepository.cs

--- src/ECER.Resources.Documents/Programs/ProgramRepository.cs
+++ src/ECER.Resources.Documents/Programs/ProgramRepository.cs
@@ -251,6 +251,7 @@ internal sealed class ProgramRepository : IProgramRepository
     
     program.StatusCode = ecer_Program_StatusCode.UnderRegistryReview;
     context.UpdateObject(program);
+    context.AddLink(pspUser, ecer_Program.Fields.ecer_ProgramRepresentative, program);
     
     context.SaveChanges();
     return id;
