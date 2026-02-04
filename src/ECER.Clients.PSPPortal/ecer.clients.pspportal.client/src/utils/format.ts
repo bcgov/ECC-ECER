681a801368132a2149f88d8fcaed8870155103ea
DSO <derek.so@gov.bc.ca>
2026-02-04 14:34:23 -0800

    ecer-5735 typescript fix for returning a string when undefined or null with format date

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/utils/format.ts
+++ src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/utils/format.ts
@@ -4,6 +4,9 @@ export function formatDate(
   inputDate: string | null | undefined,
   toFormat: DateFormat = "yyyy-MM-dd",
 ) {
+  if (inputDate === undefined || inputDate === null) {
+    return "Invalid date";
+  }
   if (inputDate) {
     const formattedDate = DateTime.fromISO(inputDate).toFormat(toFormat);
 
