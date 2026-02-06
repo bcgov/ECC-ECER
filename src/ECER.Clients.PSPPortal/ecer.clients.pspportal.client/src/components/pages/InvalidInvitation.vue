5f348f270bb89fd9b24f33661bbcbb594475e02b
Parker Atkins <parker.cattell.atkins@gmail.com>
2026-02-06 14:55:53 -0800

    Update error messages for clarity and consistency across registration components

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/components/pages/InvalidInvitation.vue
+++ src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/components/pages/InvalidInvitation.vue
@@ -1,11 +1,21 @@
 <template>
   <v-container>
     <div class="d-flex flex-column ga-10 mt-10">
-      <h1>Sorry, this link is no longer valid</h1>
+      <h1>Sorry, this link is no longer valid.</h1>
       <h2 class="mt-20">
-        This link is no longer valid. Please request a new invitation from your
-        institution administrator.
+        You may request a new invitation from another program representative at
+        your educational institution who has access to the ECER Post-Secondary
+        Programs portal.
       </h2>
+      <div class="d-flex flex-column ga-3">
+        <!-- prettier-ignore -->
+        <p>
+          If you need additional support, please contact the Registry at
+          <a href="mailto:ECERegistry.Programs@gov.bc.ca">
+            ECERegistry.Programs@gov.bc.ca
+          </a>.
+        </p>
+      </div>
       <v-btn
         class="mt-8 align-self-start"
         @click="oidcStore.logout"
