a1ed6eb5dcd0fe828a5bfd4f67d5bbf2dc0adbac
Avisha Sodhi <38086281+SodhiA1@users.noreply.github.com>
2026-02-17 14:00:50 -0800

    UT fix

--- src/ECER.Resources.Documents/ProgramApplications/IProgramApplicationRepository.cs
+++ src/ECER.Resources.Documents/ProgramApplications/IProgramApplicationRepository.cs
@@ -23,7 +23,6 @@ public record ProgramApplication(string? Id, string PostSecondaryInstituteId)
   public string? ProgramApplicationName { get; set; }
   public ApplicationType? ProgramApplicationType { get; set; }
   public ApplicationStatus? Status { get; set; }
-  public ProvincialCertificationTypeOffered? ProgramType { get; set; }
   public IEnumerable<ProgramCertificationType>? ProgramTypes { get; set; }
   public DeliveryType? DeliveryType { get; set; }
   public bool? ComponentsGenerationCompleted { get; set; }
