0cde8cde0e40896d7214cd44c0cf7c8c7fc3aa42
Parker Atkins <parker.cattell.atkins@gmail.com>
2026-03-04 15:21:09 -0800

    npm run format

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/store/user.ts
+++ src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/store/user.ts
@@ -31,7 +31,8 @@ export const useUserStore = defineStore("user", {
     role: (state): Components.Schemas.PspUserRole =>
       state.pspUserProfile?.role ?? "Primary",
     preferredName: (state): string => state.pspUserProfile?.preferredName ?? "",
-    auspice: (state): string => state.educationInstitution?.institutionType ?? "",
+    auspice: (state): string =>
+      state.educationInstitution?.institutionType ?? "",
     street1: (state): string => state.educationInstitution?.street1 ?? "",
     city: (state): string => state.educationInstitution?.city ?? "",
     province: (state): string => state.educationInstitution?.province ?? "",
