fc97642528e8b8fa892c82db6c8399c863bb5c72
Parker Atkins <parker.cattell.atkins@gmail.com>
2026-04-19 21:03:30 -0700

    Update copy under "Current program profiles"

--- src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/components/pages/ProgramProfiles.vue
+++ src/ECER.Clients.PSPPortal/ecer.clients.pspportal.client/src/components/pages/ProgramProfiles.vue
@@ -87,6 +87,31 @@
           <ECEHeader title="Current program profiles" />
         </v-col>
       </v-row>
+      <v-row>
+        <v-col cols="12">
+          <p>
+            These are the program profiles for the current and following
+            academic year. You can click "View" to see the details of a program
+            profile or update a program profile outside of the annual review
+            period if the changes do not impact program requirements or
+            competencies.
+          </p>
+          <p class="mt-3">
+            If your update will affect program requirements or competencies,
+            please
+            <router-link
+              :to="{
+                name: 'new-message',
+                params: { initialCategory: 'ProgramChangeRequest' },
+              }"
+              class="text-primary text-decoration-underline"
+            >
+              submit a change request
+            </router-link>
+            instead.
+          </p>
+        </v-col>
+      </v-row>
       <ProgramProfilesList
         :programs="currentProgramProfiles"
         @withdrawn="fetchPrograms"
